d677df6d02ee963ac311d779452ce977100a8f8f
Marco Marinho <marcomarinho12@gmail.com>
2017-12-10 13:42:00 +0100

    vendor: start oreo from clean
    * Let's keep all clean! We'll start from the beginning
    
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

---
+++ configs/phone.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2017 The Pure Nexus Project
+# Copyright (C) 2017 Euclidean OS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include vendor/eucos/configs/aosp_fixes.mk
+include vendor/eucos/configs/version.mk
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk \
+    CellBroadcastReceiver
+
+# Allow tethering without provisioning app
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dun.override=0
+
+# Pull in Prebuilt applications for phones
+$(call inherit-product-if-exists, vendor/prebuilt/prebuilt.mk)
